e1079a360e529a6583a1a596c67f0484a9add675
maladroitPancake <157930700+maladroitPancake@users.noreply.github.com>
2024-11-09 23:02:12 -0500

    Water animator implemented

--- Assets/Scripts/waterAnimator.cs
+++ Assets/Scripts/waterAnimator.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class waterAnimator : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
+    [SerializeField] Sprite[] frames;
+    [SerializeField] int fps;
+    private int numberOfFrames;
+    private float accumulator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        numberOfFrames = frames.Length;
+        accumulator = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        accumulator += Time.deltaTime * fps;
+        if (accumulator >= numberOfFrames) {accumulator -= numberOfFrames;}
+        int frame = (int)accumulator;
+        spriteRenderer.sprite = frames[frame];
     }
 }
