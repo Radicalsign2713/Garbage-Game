c4d9b1ed0ff2e1b7a1de268c279a267dcb3bb29f
maladroitPancake <157930700+maladroitPancake@users.noreply.github.com>
2024-11-11 00:00:13 -0500

    updated sprite code

--- Assets/Scripts/BatteryBar.cs
+++ Assets/Scripts/BatteryBar.cs
@@ -7,11 +7,13 @@ public class BatteryBar : MonoBehaviour
     private PlayerMovementController player;
     private float starting_percent;
     private Vector3 initial_scale;
+    private SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
         initial_scale = transform.localScale;
         player = GameObject.Find("Steve-E").GetComponent<PlayerMovementController>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
         starting_percent = player.battery;
     }
 
@@ -19,5 +21,9 @@ public class BatteryBar : MonoBehaviour
     void Update()
     {
         transform.localScale = new Vector3(initial_scale.x * player.battery / starting_percent, initial_scale.y, initial_scale.z);
+        if(player.battery / starting_percent <= .1) {spriteRenderer.color = Color.red;}
+        else if(player.battery / starting_percent <= .3) {spriteRenderer.color = Color.yellow;}
+        else {spriteRenderer.color = Color.white;}
+        
     }
 }
