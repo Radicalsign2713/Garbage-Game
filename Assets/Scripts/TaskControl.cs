b70675f3ae3bd912eb3b8d074dc5d014eda48160
maladroitPancake <157930700+maladroitPancake@users.noreply.github.com>
2024-11-11 15:00:30 -0500

    Adding garbage

--- Assets/Scripts/TaskControl.cs
+++ Assets/Scripts/TaskControl.cs
@@ -16,12 +16,17 @@ public class TaskControl : MonoBehaviour
     {
         if(tag == "Garbage") {collected_garbages ++;}
         else if(tag == "Upgrade") {collected_upgrades ++;}
+        if(total_garbages == collected_garbages & total_upgrades == collected_upgrades) {tasks_completed = true;}
     }
 
     // Start is called before the first frame update
     void Start()
     {
         tasks_completed = false;
+        collected_garbages = 0;
+        collected_upgrades = 0;
+        total_garbages = GameObject.FindGameObjectsWithTag("Garbage").Length;
+        total_upgrades = GameObject.FindGameObjectsWithTag("Upgrade").Length;
     }
     private void UPdateUI()
     {
